7eb84b2dec3a0f26b578d7b817bd0ed9be8e3358
vidyanandtange <vidyanandtange@gmail.com>
2024-09-28 12:20:48 +0530

    user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,7 @@
-import mongoose, {Schema} from 'mongoose'
+import mongoose, {Schema} from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
 
 const userSchema = new Schema({
   username: {
@@ -42,5 +45,40 @@ const userSchema = new Schema({
   }
 
 }, {timestamps:true})
+
+// pre() is middleware hooks
+userSchema.pre("save", async function(next){
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+})
+
+// create custom methods 
+userSchema.methods.isPasswordCorrect = async function(password) {
+  return await bcrypt.compare(password, this.password)
+}
+// sign() generating token
+userSchema.methods.generateAccessToken = function() {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+userSchema.methods.generateRefreshToken = function() {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+  )
+}
+
 
 export const User = mongoose.model("User", userSchema)
